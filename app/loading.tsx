7049204de778db4a4eb2f32e39ebdcd4b6f573e2
atharvaujoshicric-dev <atharvaujoshicric@gmail.com>
2026-07-17 19:01:59 +0530

    Delete app directory

--- app/loading.tsx
+++
@@ -1,8 +0,0 @@
-// app/loading.tsx
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen items-center justify-center">
-      <div className="h-8 w-8 animate-spin rounded-full border-2 border-border border-t-accent" />
-    </div>
-  );
-}
