da2566298a5f41b957e2c013272336b38b2b3652
atharvaujoshicric-dev <atharvaujoshicric@gmail.com>
2026-07-17 19:02:10 +0530

    Delete components directory

--- components/providers/theme-provider.tsx
+++
@@ -1,9 +0,0 @@
-// components/providers/theme-provider.tsx
-'use client';
-
-import * as React from 'react';
-import { ThemeProvider as NextThemesProvider, type ThemeProviderProps } from 'next-themes';
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
